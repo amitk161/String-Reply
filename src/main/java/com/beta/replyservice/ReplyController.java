6392848f59ab6a16613fbcae8ef3d091bf567a1b
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2023-11-29 10:51:41 +0530

    string reply service

--- src/main/java/com/beta/replyservice/ReplyController.java
+++ src/main/java/com/beta/replyservice/ReplyController.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Arrays;
+
 @RestController
 public class ReplyController {
+
+	ReplyMessage replyMessage = new ReplyMessage("");
+
 
 	@GetMapping("/reply")
 	public ReplyMessage replying() {
@@ -16,4 +21,26 @@ public class ReplyController {
 	public ReplyMessage replying(@PathVariable String message) {
 		return new ReplyMessage(message);
 	}
+
+
+	// V2 mapping for the new api endpoint for the string reply
+	@GetMapping("/v2/reply/{message}")
+	public String replyingV2(@PathVariable String message) {
+		String[] arrOfStr = message.split("-");
+		String ruleSet = arrOfStr[0];
+		String messageInput = arrOfStr[1];
+		char [] rules = ruleSet.toCharArray();
+
+		String result = messageInput;
+
+		for (int i = 0; i < rules.length; i++) {
+			if (rules[i] == '1'){
+				result =  replyMessage.reverseString(result,1);
+			} else if (rules[i] == '2') {
+				result =  replyMessage.encode(message,2);
+			} else return "Invalid Input";
+		}
+		return result;
+	}
+
 }
--- src/main/java/com/beta/replyservice/ReplyMessage.java
+++ src/main/java/com/beta/replyservice/ReplyMessage.java
@@ -1,4 +1,7 @@
 package com.beta.replyservice;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ReplyMessage {
 
@@ -10,5 +13,41 @@ public class ReplyMessage {
 
 	public String getMessage() {
 		return message;
+	}
+
+	// function written to reverse the String.
+	public String reverseString(String inputMessage, int rule){
+		StringBuilder result = new StringBuilder();
+		if (rule == 1){
+			result.append(inputMessage);
+			result.reverse();
+		}
+		return result.toString();
+	}
+
+	// function written to encode the string via MD5 hash algorithm.
+	public String encode(String input,int rule) {
+		if (rule == 2){
+			try {
+				byte[] bytesOfMessage = input.getBytes("UTF-8");
+				MessageDigest md = MessageDigest.getInstance("MD5");
+				byte[] thedigest = md.digest(bytesOfMessage);
+				return convertByteArrayToHexString(thedigest);
+			} catch (UnsupportedEncodingException e) {
+				System.out.println("MD5.encode() threw an UnsupportedEncodingException");
+			} catch (NoSuchAlgorithmException e) {
+				System.out.println("MD5.encode() threw an NoSuchAlgorithmException");
+			}
+		}
+		return null;
+	}
+
+	// function to display the converted hash and display as hex.
+	private String convertByteArrayToHexString(byte[] arrayBytes) {
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < arrayBytes.length; i++) {
+			stringBuffer.append(Integer.toString((arrayBytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return stringBuffer.toString();
 	}
 }
